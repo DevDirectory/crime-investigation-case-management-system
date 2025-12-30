0ad2acbe112c27278b582eb889651b324d4ce8e7
Eden Alemayehu Gebeta <edengebeta210@gmail.com>
2025-12-30 13:28:42 +0300

    Fix casemanager

--- Cases/CaseManager.cpp
+++ Cases/CaseManager.cpp
@@ -4,27 +4,31 @@
 
 using namespace std;
 
-struct Case {
+struct Case
+{
     int id;
     char title[50];
     char status[20];
-    Case* next;
+    Case *next;
 };
 
-Case* head = NULL;
+Case *head = NULL;
 
-void saveCasesToFile() {
+void saveCasesToFile()
+{
     ofstream file("data/cases.txt");
-    Case* t = head;
-    while (t) {
+    Case *t = head;
+    while (t)
+    {
         file << t->id << " " << t->title << " " << t->status << endl;
         t = t->next;
     }
     file.close();
 }
 
-void addCase() {
-    Case* n = new Case;
+void addCase()
+{
+    Case *n = new Case;
     cout << "Case ID: ";
     cin >> n->id;
     cout << "Title: ";
@@ -36,25 +40,51 @@ void addCase() {
     head = n;
 
     saveCasesToFile();
-    cout<<"Case Added!";
+    cout << "Case Added!";
 }
 
-void viewCase() {
-    Case* t = head;
-    while (t) {
-        cout << t->id << " " << t->title << " " << t->status << endl;
+void viewCase(bool isAdmin)
+{
+    Case *t = head;
+
+    if (isAdmin)
+    {
+        while (t)
+        {
+            cout << t->id << " " << t->title << " " << t->status << endl;
+            t = t->next;
+        }
+        return;
+    }
+
+    int id;
+    cout << "Enter Case ID to view: ";
+    cin >> id;
+
+    while (t)
+    {
+        if (t->id == id)
+        {
+            cout << t->id << " " << t->title << " " << t->status << endl;
+            return;
+        }
         t = t->next;
     }
+
+    cout << "Case not found\n";
 }
 
-void updateCase() {
+void updateCase()
+{
     int id;
     cout << "Enter Case ID to update: ";
     cin >> id;
 
-    Case* t = head;
-    while (t) {
-        if (t->id == id) {
+    Case *t = head;
+    while (t)
+    {
+        if (t->id == id)
+        {
             cout << "New Title: ";
             cin >> t->title;
             cout << "New Status: ";
@@ -68,22 +98,27 @@ void updateCase() {
     cout << "Case not found\n";
 }
 
-void deleteCase() {
+void deleteCase()
+{
     int id;
     cout << "Enter Case ID to delete: ";
     cin >> id;
 
     Case *t = head, *p = NULL;
 
-    while (t && t->id != id) {
+    while (t && t->id != id)
+    {
         p = t;
         t = t->next;
     }
 
-    if (!t) return;
+    if (!t)
+        return;
 
-    if (!p) head = t->next;
-    else p->next = t->next;
+    if (!p)
+        head = t->next;
+    else
+        p->next = t->next;
 
     delete t;
     saveCasesToFile();
