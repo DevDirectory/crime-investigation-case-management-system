04162289c767fa9333d6fc08eadd3ccc15048a36
edenmengste <edenmengste@gmail.com>
2025-12-30 10:52:14 +0300

    Refactor admin menu: Enhance layout and input validation for better user experience

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstring>
+#include <iomanip>
 
 #include "Auth/AuthManager.h"
 #include "Cases/case.h"
@@ -17,35 +18,48 @@ void adminMenu() {
     int c;
     do {
         cout << "\n--- ADMIN MENU ---\n";
-        cout << "1 Add Case\n2 Update Case\n3 Delete Case\n4 View Case\n";
-        cout << "5 Add Suspect\n6 Update Suspect\n7 Delete Suspect\n8 View Suspect\n";
-        cout << "9 Add Officer\n10 View Officer\n11 Assign Investigator\n";
-        cout << "12 Add Evidence\n13 Update Evidence Status\n14 View Evidence\n";
-        cout << "15 Change Password\n0 Logout\nChoice: ";
-        cin >> c;
+        cout << left << setw(28) << "  1. Add Case"           << "2. Update Case" << endl;
+        cout << left << setw(28) << "  3. Delete Case"        << "4. View Case" << endl;
+        cout << left << setw(28) << "  5. Add Suspect"        << "6. Update Suspect" << endl;
+        cout << left << setw(28) << "  7. Delete Suspect"     << "8. View Suspect" << endl;
+        cout << left << setw(28) << "  9. Add Officer"        << "10. View Officer" << endl;
+        cout << left << setw(28) << "  11. Assign Investigator" << "12. Add Evidence" << endl;
+        cout << left << setw(28) << "  13. Update Evidence"   << "14. View Evidence" << endl;
+        cout << left << setw(28) << "  15. Change Password"   << "0. Logout" << endl;
+        cout << "Choice: ";
+
+        if (!(cin >> c)) {
+            cout << "\n[!] Invalid input! Please enter a number.\n";
+            cin.clear(); 
+            cin.ignore(1000, '\n'); 
+            c = -1;
+            continue;
+        } 
 
         switch (c) {
             case 1: addCase(); break;
             case 2: updateCase(); break;
             case 3: deleteCase(); break;
             case 4: viewCase(); break;
-
             case 5: addSuspect(); break;
             case 6: updateSuspect(); break;
             case 7: deleteSuspect(); break;
             case 8: viewSuspect(); break;
-
             case 9: addOfficer(); break;
             case 10: viewOfficer(); break;
             case 11: assignInvestigator(); break;
-
             case 12: addEvidence(); break;
             case 13: updateInvestigationStatus(); break;
             case 14: viewEvidence(); break;
             case 15: changePassword(); break;
+            case 0: cout << "Logging out...\n"; break;
+            default:
+                cout << "\n[!] " << c << " is not a valid option. Try 0-15.\n";
+                break;
         }
     } while (c != 0);
 }
+
 
 void officerMenu() {
     int c;
